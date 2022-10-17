8a2bdd4600f6d079e4bb84a5f78fe887b9b2938c
SushantParicharak <sushant.paricharak@gmail.com>
2022-10-17 19:07:15 -0400

    Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module aws_go_snowflake
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main(){
+	var welcome string = "Hello World"
+	//welcome :=  "Hello World" 
+	println(welcome)
+
+	//short variable declaration
+	myage := 23
+    myage -= 10
+	println(myage)
+
+	const pi float32 = 3.14
+	println(pi)
+
+	var size float32 = 1.5
+	println(pi * size)
+}
